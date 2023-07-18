b0e6e236d6709d17abf2f076bdf39c4f35737cac
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-07-18 23:11:41 +0500

    crud operation done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # crud-api
+
+Run Nodemon 
+first install nodemon
+and run in cmd like npm run dev
--- server.mjs
+++ server.mjs
@@ -1,11 +1,182 @@
 import express from "express";
+import { customAlphabet } from 'nanoid';
+const nanoid = customAlphabet('1234567890', 20);
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello world! by Atif1");
+  res.send("Hello world! by Atif");
 });
+
+
+// Create sample product
+let products = [
+  {
+    id: nanoid(), //number
+    name: "Item one",
+    price: "$8.14",
+    description: "Item description here..."
+  }
+];
+
+
+
+// get all list of products
+app.get("/products", (req, res) => {
+  res.send({
+    message: "All Products",
+    data: products
+  });
+});
+
+
+
+
+// get single product
+app.get("/product/:id", (req, res) => {
+  console.log(typeof req.params.id)
+
+  if (isNaN(req.params.id)) {
+    res.status(403).send("invalid product id")
+  }
+
+  let isFound = false;
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === req.params.id) {
+      isFound = i;
+      break;
+    }
+  }
+
+  if (isFound === false) {
+    res.status(404);
+    res.send({
+      message: "product not found"
+    });
+  } else {
+    res.send({
+      message: "product found with id: " + products[isFound].id,
+      data: products[isFound]
+    });
+  }
+});
+
+
+
+// post a product
+app.post("/product", (req, res) => {
+
+  // {
+  //   id: 212342, // always a number
+  //   name: "abc product",
+  //   price: "$23.12",
+  //   description: "abc product description"
+  // }
+
+
+  if (!req.body.name
+    || !req.body.price
+    || !req.body.description) {
+
+    res.status(403).send(`
+      required parameter missing. example JSON request body:
+      {
+        name: "abc product",
+        price: "$23.12",
+        description: "abc product description"
+      }`);
+  }
+
+  products.push({
+    id: nanoid(),
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+  });
+
+
+  res.status(201).send({ message: "created product" });
+});
+
+
+
+
+// Edit single product
+app.put("/product/:id", (req, res) => {
+
+  if (
+    !req.body.name
+    && !req.body.price
+    && !req.body.description) {
+
+    res.status(403).send(`
+      required parameter missing. 
+      atleast one parameter is required: name, price or description to complete update
+      example JSON request body:
+      {
+        name: "abc product",
+        price: "$23.12",
+        description: "abc product description"
+      }`);
+  }
+
+
+  let isFound = false;
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === req.params.id) {
+      isFound = i;
+      break;
+    }
+  }
+
+  if (isFound === false) {
+    res.status(404);
+    res.send({
+      message: "product not found"
+    });
+  } else {
+
+    if (req.body.name) products[isFound].name = req.body.name
+    if (req.body.price) products[isFound].price = req.body.price
+    if (req.body.description) products[isFound].description = req.body.description
+
+    res.send({
+      message: "product is updated with id: " + products[isFound].id,
+      data: products[isFound]
+    });
+  }
+});
+
+
+// Delete single product
+app.delete("/product/:id", (req, res) => {
+
+  let isFound = false;
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === req.params.id) {
+      isFound = i;
+      break;
+    }
+  }
+
+  if (isFound === false) {
+    res.status(404);
+    res.send({
+      message: "product not found"
+    });
+  } else {
+    products.splice(isFound, 1)
+
+    res.send({
+      message: "product is deleted"
+    });
+  }
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
